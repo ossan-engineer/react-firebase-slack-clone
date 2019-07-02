d95f6ea065f6a2606eb0fb0a2f7b154506ba3680
ossan-engineer <bodega1123@gmail.com>
2019-07-02 19:23:37 +0900

    Use shorthand for collection

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -5,9 +5,7 @@ const Messages = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    db.collection('channels')
-      .doc('general')
-      .collection('messages')
+    db.collection('channels/general/messages')
       .orderBy('createdAt')
       .onSnapshot(snapshot => {
         const docs: any = [];

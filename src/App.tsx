5123c6e8cee72e83c96488cb84848af17e558ac7
ossan-engineer <bodega1123@gmail.com>
2019-07-03 19:37:27 +0900

    Implement login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './Nav';
 import Channel from './Channel';
+import { firebase } from './firebase';
 
 const App: React.FC = () => {
-  return (
+  const [user, setUser] = useState<any>(null);
+
+  const handleSignIn = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await firebase.auth().signInWithPopup(provider);
+  };
+
+  console.log(user);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  return user ? (
     <div className="App">
       <Nav />
       <Channel />
+    </div>
+  ) : (
+    <div className="Login">
+      <h1>Chat!</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
     </div>
   );
 };

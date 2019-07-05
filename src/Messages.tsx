4af9a65c1607662cd3550d66f87857a005710a2c
ossan-engineer <bodega1123@gmail.com>
2019-07-05 21:06:37 +0900

    Display author on messages

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useCollection from './useCollection';
+import { db } from './firebase';
 
-const FirstMessageFromUser = ({ message, showDay }: any) => (
-  <div>
-    {showDay && (
-      <div className="Day">
-        <div className="DayLine" />
-        <div className="DayText">12/6/2018</div>
-        <div className="DayLine" />
-      </div>
-    )}
-    <div className="Message with-avatar">
-      <div className="Avatar" />
-      <div className="Author">
-        <div>
-          <span className="UserName">Ryan Florence </span>
-          <span className="TimeStamp">3:37 PM</span>
+const useDoc = (path: string) => {
+  const [doc, setDoc] = useState<any>(null);
+
+  useEffect(() => {
+    return db.doc(path).onSnapshot(doc => {
+      setDoc({
+        ...doc.data(),
+        id: doc.id,
+      });
+    });
+  }, []);
+
+  return doc;
+};
+
+const FirstMessageFromUser = ({ message, showDay }: any) => {
+  const author = useDoc(message.user.path);
+  return (
+    <div>
+      {showDay && (
+        <div className="Day">
+          <div className="DayLine" />
+          <div className="DayText">12/6/2018</div>
+          <div className="DayLine" />
         </div>
-        <div className="MessageContent">{message.text}</div>
+      )}
+      <div className="Message with-avatar">
+        <div
+          className="Avatar"
+          style={{ backgroundImage: author ? `url("${author.photoUrl}")` : '' }}
+        />
+        <div className="Author">
+          <div>
+            <span className="UserName">{author && author.displayName}</span>
+            <span className="TimeStamp">3:37 PM</span>
+          </div>
+          <div className="MessageContent">{message.text}</div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Messages = () => {
   const messages = useCollection('/channels/general/messages', 'createdAt');

cde69fd1ac8d8b22972318015e7007079ed03969
ossan-engineer <bodega1123@gmail.com>
2019-07-05 20:47:16 +0900

    Refactor messages

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import useCollection from './useCollection';
 
+const FirstMessageFromUser = ({ message, showDay }: any) => (
+  <div>
+    {showDay && (
+      <div className="Day">
+        <div className="DayLine" />
+        <div className="DayText">12/6/2018</div>
+        <div className="DayLine" />
+      </div>
+    )}
+    <div className="Message with-avatar">
+      <div className="Avatar" />
+      <div className="Author">
+        <div>
+          <span className="UserName">Ryan Florence </span>
+          <span className="TimeStamp">3:37 PM</span>
+        </div>
+        <div className="MessageContent">{message.text}</div>
+      </div>
+    </div>
+  </div>
+);
+
 const Messages = () => {
   const messages = useCollection('/channels/general/messages', 'createdAt');
   return (
@@ -8,24 +30,11 @@ const Messages = () => {
       <div className="EndOfMessages">That's every message!</div>
 
       {messages.map((message: any, index: number) => {
-        return index === 0 ? (
-          <div>
-            <div className="Day">
-              <div className="DayLine" />
-              <div className="DayText">12/6/2018</div>
-              <div className="DayLine" />
-            </div>
-            <div className="Message with-avatar">
-              <div className="Avatar" />
-              <div className="Author">
-                <div>
-                  <span className="UserName">Ryan Florence </span>
-                  <span className="TimeStamp">3:37 PM</span>
-                </div>
-                <div className="MessageContent">{message.text}</div>
-              </div>
-            </div>
-          </div>
+        const previous: any = messages[index - 1];
+        const showDay = false;
+        const showAvatar = !previous || message.user.id !== previous.user.id;
+        return showAvatar ? (
+          <FirstMessageFromUser message={message} showDay={showDay} />
         ) : (
           <div>
             <div className="Message no-avatar">

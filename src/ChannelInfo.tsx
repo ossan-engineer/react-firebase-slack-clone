8255d9077f9767b24a7675c8ce011d0d898b1d4f
ossan-engineer <bodega1123@gmail.com>
2019-07-09 20:20:16 +0900

    Add readOnly

--- src/ChannelInfo.tsx
+++ src/ChannelInfo.tsx
@@ -4,7 +4,7 @@ const ChannelInfo = () => {
   return (
     <div className="ChannelInfo">
       <div className="Topic">
-        Topic: <input className="TopicInput" value="Awesome stuff" />
+        Topic: <input className="TopicInput" value="Awesome stuff" readOnly />
       </div>
       <div className="ChannelName">#general</div>
     </div>

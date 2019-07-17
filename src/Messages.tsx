f421494887ad7283f15a6e753789f2f15dd6bed9
ossan-engineer <bodega1123@gmail.com>
2019-07-17 12:19:11 +0900

    Avoid flickers with simple cache

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -2,18 +2,28 @@ import React, { useState, useEffect } from 'react';
 import useCollection from './useCollection';
 import { db } from './firebase';
 
+const cache: any = {};
+const pendingCache: any = {};
+
 const useDoc = (path: string) => {
-  const [doc, setDoc] = useState<any>(null);
+  const [doc, setDoc] = useState<any>(cache[path]);
 
   useEffect(() => {
-    db.doc(path)
-      .get()
-      .then(doc => {
-        setDoc({
-          ...doc.data(),
-          id: doc.id,
-        });
-      });
+    if (doc) {
+      return;
+    }
+
+    const pending = pendingCache[path];
+    const promise = pending || (pendingCache[path] = db.doc(path).get());
+
+    promise.then((doc: any) => {
+      const user = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setDoc(user);
+      cache[path] = user;
+    });
   }, [path]);
 
   return doc;

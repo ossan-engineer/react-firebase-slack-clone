8684d1566575105bd3cb36dd2e7e03d03d7166c7
ossan-engineer <bodega1123@gmail.com>
2019-07-25 22:57:08 +0900

    Deploy functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,13 @@ import * as functions from 'firebase-functions';
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+  console.log('hello there!');
+  if (Math.random() > 0.25) {
+    console.error('Blow up!');
+  } else if (Math.random() > 0.25) {
+    throw new Error('hey');
+  } else {
+    console.log('hey');
+  }
+  response.send('Hello with errors and stuff!');
 });

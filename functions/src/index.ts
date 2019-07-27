2e9462c7f9c89fc2a427bf465a9e6a1b98d3e1f3
ossan-engineer <bodega1123@gmail.com>
2019-07-27 16:56:58 +0900

    Sync status from RTDB  to Firestore serverside

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,31 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+
+exports.onUserStatusChanged = functions.database
+  .ref('status/{userId}')
+  .onUpdate((change, context) => {
+    const eventStatus = change.after.val();
+    const userDoc = db.doc(`users/${context.params.userId}`);
+
+    return change.after.ref.once('value').then(snapshot => {
+      const status = snapshot.val();
+      if (status.lashChanged > eventStatus.lashChanged) {
+        return;
+      }
+
+      eventStatus.lashChanged = new Date(eventStatus.lastChanged);
+      userDoc.update({
+        status: eventStatus,
+      });
+    });
+  });
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   console.log('hello there!');
